567c721e727cc571178f8f73cf1a0c024a6b466f
Lenny Onyango Ochanda <lennyonduto384@gmail.com>
2022-11-05 12:26:28 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+  import Avatar from "./lib/Avatar.svelte";
+  import Social from "./lib/Social.svelte";
+  import Footer from "./lib/Footer.svelte";
+</script>
+
+<main>
+  <Avatar />
+  <h1>Hello world!</h1>
+  <p class="desc">
+    <span>Lenny Ochanda</span> is a web developer, Commerce (Finance) student at Kenyatta University, musician, avid reader and defender of human rights.
+    He can be found on:
+  </p>
+  <Social />
+</main>
+<footer>
+  <Footer />
+</footer>
+
+<style>
+  .desc {
+    color: rgb(136, 136, 136);
+  }
+  
+  span {
+    color: rgb(33, 53, 71);
+  }
+
+  footer {
+    position: sticky;
+    width: 100%;
+    bottom: 0;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+  }
+</style>
---
+++ src/lib/Footer.svelte
@@ -0,0 +1,7 @@
+<p>&#169; Lenny Onyango Ochanda | 2022</p>
+
+<style>
+  p {
+    font-size: .8em;
+  }
+</style>
